c5e1666fd060942fb2c69942427d402234368cdd
tejaswinigerikoti <tejaswinigerikoti@gmail.com>
2025-12-17 14:31:40 +0530

    Add files via upload

---
+++ Pattern5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Pattern5 {
+    public static void main(String[] args) {
+        int n=11;
+        for (int i = 0; i<n; i++) {
+            for(int j=0;j<n;j++){
+                if(i==0||i==n-1||j==0||j==n-1||i==5||j==5){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
