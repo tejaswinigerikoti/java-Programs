0aadd87480c8def87981eb2d0f7e1e6cc94728c0
tejaswinigerikoti <tejaswinigerikoti@gmail.com>
2025-12-17 11:02:45 +0530

    Add files via upload

---
+++ Exampleforconditional.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Exampleforconditional{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        if(age>=18){
+            System.out.println("Eligible");
+        }
+    }
+}
